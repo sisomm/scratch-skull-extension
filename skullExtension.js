76a0dbd35d222a18509673933caf7209479a9992
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2016-02-10 22:09:38 +0100

    hei

--- skullExtension.js
+++ skullExtension.js
@@ -53,4 +53,40 @@
     // Register the extension
     ScratchExtensions.register('Keith Richards', descriptor, ext);
     loadMQTT();
+
+    var wsbroker = "test.mosquitto.org";  //mqtt websocket enabled broker
+    var wsport = 8080 // port for above
+    var client = new Paho.MQTT.Client(wsbroker, wsport,
+        "myclientid_" + parseInt(Math.random() * 100, 10));
+    
+    client.onConnectionLost = function (responseObject) {
+        console.log("connection lost: " + responseObject.errorMessage);
+    };
+    
+    client.onMessageArrived = function (message) {
+        console.log(message.destinationName, ' -- ', message.payloadString);
+    };
+    
+    var options = {
+      timeout: 3,
+      onSuccess: function () {
+        console.log("mqtt connected");
+        // Connection succeeded; subscribe to our topic, you can add multile lines of these
+        client.subscribe('/World', {qos: 1});
+    
+        //use the below if you want to publish to a topic on connect
+        message = new Paho.MQTT.Message("Hello");
+        message.destinationName = "/World";
+        client.send(message);
+  
+      },
+      onFailure: function (message) {
+        console.log("Connection failed: " + message.errorMessage);
+      }
+    };
+      
+
+    client.connect(options);
+
+
 })({});
