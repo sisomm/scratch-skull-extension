0cd1ac8362eedbcace9ca3b1631f9932875a8e48
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2016-02-10 19:21:05 +0100

    first

---
+++ skullExtension.js
@@ -0,0 +1,38 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Functions for block with type 'w' will get a callback function as the 
+    // final argument. This should be called to indicate that the block can
+    // stop waiting.
+    ext.wait_random = function(callback) {
+        wait = Math.random();
+        console.log('Waiting for ' + wait + ' seconds');
+        window.setTimeout(function() {
+            callback();
+        }, wait*1000);
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['w', 'Blink Eyes', 'wait_random'],
+            ['w', 'Open Mouth', 'wait_random'],
+            ['w', 'Close Mouth', 'wait_random'],
+            ['w', 'Move Head', 'wait_random'],
+            ['w', 'Talk', 'wait_random'],
+            ['w', 'Left Eye', 'wait_random'],
+            ['w', 'Right Eye', 'wait_random'],
+            ['w', 'Talk', 'wait_random'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Keith Richards', descriptor, ext);
+})({});
