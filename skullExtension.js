4573b99d1b4730cdab4d86062401b7b9eba429c6
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2016-02-11 21:40:12 +0100

    hei

--- skullExtension.js
+++ skullExtension.js
@@ -25,6 +25,16 @@
     // Functions for block with type 'w' will get a callback function as the 
     // final argument. This should be called to indicate that the block can
     // stop waiting.
+
+    ext.blink_eyes = function() {
+        console.log("blink");
+        message = new Paho.MQTT.Message("BLINK,9");
+        message.destinationName = mqqtDefaultTopic;
+        client.send(message);
+        console.log("I blank");
+
+    }
+
     ext.wait_random = function(callback) {
         wait = Math.random();
         console.log('Waiting for ' + wait + ' seconds');
@@ -36,15 +46,16 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            [' ', 'Blink Eyes', 'wait_random'],
-            ['w', 'Mouth %m.openClose', 'wait_random'],
-            ['w', 'Move Head to %n,%n', 'wait_random',300,300],
-            ['w', 'Talk %n times', 'wait_random',5],
-            ['w', 'Left Eye %m.onOff', 'wait_random'],
-            ['w', 'Right Eye %m.onOff', 'wait_random'],
+            [' ', 'Blink Eyes', 'blink_eyes'],
+            [' ', 'Mouth %m.openClose', 'wait_random'],
+            [' ', 'Move Head to %n,%n', 'wait_random',300,300],
+            [' ', 'Talk %n times', 'wait_random',5],
+            [' ', '%m.whichEye Eye %m.onOff', 'wait_random'],
+            [' ', 'MQTT topic %s message %s','send_mqtt','/scratch/sisomm','Hello, World'],
         ], 
         menus: {
             openClose:['Open','Close'],
+            whichEye:['Left','Right'],
             onOff:['On','Off']
         }
     };
@@ -57,6 +68,7 @@
     var wsport = 8080 // port for above
     var client = new Paho.MQTT.Client(wsbroker, wsport,
         "myclientid_" + parseInt(Math.random() * 100, 10));
+    var mqqtDefaultTopic='/home/sisomm';
     
     client.onConnectionLost = function (responseObject) {
         console.log("connection lost: " + responseObject.errorMessage);
@@ -71,11 +83,11 @@
       onSuccess: function () {
         console.log("mqtt connected");
         // Connection succeeded; subscribe to our topic, you can add multile lines of these
-        client.subscribe("/arduino/1/incoming", {qos: 1});
+        client.subscribe("/scratch/sisomm/#", {qos: 1});
     
         //use the below if you want to publish to a topic on connect
         message = new Paho.MQTT.Message('Hello');
-        message.destinationName = "/arduino/1/incoming";
+        message.destinationName = "/scratch/sisomm";
         client.send(message);
   
       },
